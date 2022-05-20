167b1cb61a94a36b78373e0d5b632ee005b2376a
Blackcoffee69420 <105739014+Blackcoffee69420@users.noreply.github.com>
2022-05-21 01:23:36 +0530

    Update checks.py

--- dismusic/checks.py
+++ dismusic/checks.py
@@ -1,14 +1,12 @@
 from discord.ext import commands
-import nextcord
 
 from .errors import MustBeSameChannel, NotConnectedToVoice, PlayerNotConnected
 
 
 def voice_connected():
     def predicate(ctx: commands.Context):
-        em=nextcord.Embed(title="Error",description="You are not connected to any voice channel.")
         if not ctx.author.voice:
-            raise NotConnectedToVoice(embed=em)
+            raise NotConnectedToVoice("You are not connected to any voice channel.")
 
         return True
 
@@ -18,16 +16,13 @@ def voice_connected():
 def voice_channel_player():
     def predicate(ctx: commands.Context):
         if not ctx.author.voice:
-            em=nextcord.Embed(title="Error",description="You are not connected to any voice channel.")
-            raise NotConnectedToVoice(embed=em)
+            raise NotConnectedToVoice("You are not connected to any voice channel.")
 
         if not ctx.voice_client:
-            em=nextcord.Embed(title="Error",description="Player is connected to any voice channel.")
-            raise PlayerNotConnected(embed=em)
+            raise PlayerNotConnected("Player is connected to any voice channel.")
 
         if ctx.voice_client.channel.id != ctx.author.voice.channel.id:
-            em=nextcord.Embed(title="Error",description="You must be in the same voice channel as the player.")
-            raise MustBeSameChannel(embed=em)
+            raise MustBeSameChannel("You must be in the same voice channel as the player.")
 
         return True
 
