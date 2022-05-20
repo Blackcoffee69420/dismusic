db150b5ef933a9296482f2867fda0597e384037d
Blackcoffee69420 <105739014+Blackcoffee69420@users.noreply.github.com>
2022-05-21 01:15:41 +0530

    Update checks.py

--- dismusic/checks.py
+++ dismusic/checks.py
@@ -17,15 +17,16 @@ def voice_connected():
 def voice_channel_player():
     def predicate(ctx: commands.Context):
         if not ctx.author.voice:
-            raise NotConnectedToVoice("You are not connected to any voice channel.")
+            em=discord.Embed(title="Error",description="You are not connected to any voice channel.")
+            raise NotConnectedToVoice(embed=em)
 
         if not ctx.voice_client:
-            raise PlayerNotConnected("Player is not connected to any voice channel.")
+            em=discord.Embed(title="Error",description="Player is connected to any voice channel.")
+            raise PlayerNotConnected(embed=em)
 
         if ctx.voice_client.channel.id != ctx.author.voice.channel.id:
-            raise MustBeSameChannel(
-                "You must be in the same voice channel as the player."
-            )
+            em=discord.Embed(title="Error",description="You must be in the same voice channel as the player.")
+            raise MustBeSameChannel(embed=em)
 
         return True
 
